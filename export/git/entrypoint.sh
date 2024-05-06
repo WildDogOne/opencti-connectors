fc8591c7e541068475be59d0b1dee5657dd8aeb6
Linus <element.oxygen@gmail.com>
2024-05-06 19:09:25 +0200

    adding git export in multiple formats

---
+++ export/git/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Go to the right directory
+cd /opt/opencti-connector-export-git
+
+# Launch the worker
+python3 main.py
---
+++ export/git/src/main.py
@@ -0,0 +1,445 @@
+import csv
+import io
+import json
+import os
+import sys
+import time
+from datetime import datetime, timezone
+
+import yaml
+from pycti import OpenCTIConnectorHelper, get_config_variable
+
+
+class ExportGit:
+    def __init__(self):
+        # Instantiate the connector helper from config
+        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
+        config = (
+            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
+            if os.path.isfile(config_file_path)
+            else {}
+        )
+        self.helper = OpenCTIConnectorHelper(config)
+        self.interval = (
+            get_config_variable(
+                "CONNECTOR_INTERVAL",
+                ["connector", "interval"],
+                config,
+                True,
+            )
+            * 60
+        )
+        self.export_file_csv_delimiter = get_config_variable(
+            "EXPORT_FILE_CSV_DELIMITER",
+            ["export-file-csv", "delimiter"],
+            config,
+            False,
+            ",",
+        )
+
+    def export_dict_list_to_csv(self, data):
+        output = io.StringIO()
+        headers = sorted(set().union(*(d.keys() for d in data)))
+        if "hashes" in headers:
+            headers = headers + [
+                "hashes.MD5",
+                "hashes_SHA-1",
+                "hashes_SHA-256",
+                "hashes_SHA-512",
+                "hashes_SSDEEP",
+            ]
+        csv_data = [headers]
+        for d in data:
+            row = []
+            for h in headers:
+                if h.startswith("hashes_") and "hashes" in d:
+                    hashes = {}
+                    for hash in d["hashes"]:
+                        hashes[hash["algorithm"]] = hash["hash"]
+                    if h.split("_")[1] in hashes:
+                        row.append(hashes[h.split("_")[1]])
+                    else:
+                        row.append("")
+                elif h not in d:
+                    row.append("")
+                elif isinstance(d[h], str):
+                    row.append(d[h])
+                elif isinstance(d[h], int):
+                    row.append(str(d[h]))
+                elif isinstance(d[h], list):
+                    if len(d[h]) > 0 and isinstance(d[h][0], str):
+                        row.append(",".join(d[h]))
+                    elif len(d[h]) > 0 and isinstance(d[h][0], dict):
+                        rrow = []
+                        for r in d[h]:
+                            if "name" in r:
+                                rrow.append(r["name"])
+                            elif "definition" in r:
+                                rrow.append(r["definition"])
+                            elif "value" in r:
+                                rrow.append(r["value"])
+                            elif "observable_value" in r:
+                                rrow.append(r["observable_value"])
+                        row.append(",".join(rrow))
+                    else:
+                        row.append("")
+                elif isinstance(d[h], dict):
+                    if "name" in d[h]:
+                        row.append(d[h]["name"])
+                    elif "value" in d[h]:
+                        row.append(d[h]["value"])
+                    elif "observable_value" in d[h]:
+                        row.append(d[h]["observable_value"])
+                    else:
+                        row.append("")
+                else:
+                    row.append("")
+            csv_data.append(row)
+        writer = csv.writer(
+            output,
+            delimiter=self.export_file_csv_delimiter,
+            quotechar='"',
+            quoting=csv.QUOTE_ALL,
+        )
+        writer.writerows(csv_data)
+        return output.getvalue()
+
+    def _process_message(self, data):
+        file_name = data["file_name"]
+        export_scope = data["export_scope"]  # query or selection or single
+        export_type = data["export_type"]  # Simple or Full
+        file_markings = data["file_markings"]
+        entity_id = data.get("entity_id")
+        entity_type = data["entity_type"]
+        main_filter = data.get("main_filter")
+        access_filter = data.get("access_filter")
+
+        if export_scope == "single":
+            self.helper.connector_logger.info(
+                "Exporting",
+                {
+                    "entity_id": entity_id,
+                    "export_type": export_type,
+                    "file_name": file_name,
+                },
+            )
+            entity_data = self.helper.api_impersonate.stix_domain_object.read(
+                id=entity_id
+            )
+            if entity_data is None:
+                entity_data = self.helper.api_impersonate.stix_cyber_observable.read(
+                    id=entity_id
+                )
+            if entity_data is None:
+                raise ValueError(
+                    "Unable to read/access to the entity, please check that the connector permission. Please note that all export files connectors should have admin permission as they impersonate the user requesting the export to avoir data leak."
+                )
+            entities_list = []
+            object_ids = entity_data.get("objectsIds")
+            if object_ids is not None and len(object_ids) != 0:
+                export_selection_filter = {
+                    "mode": "and",
+                    "filterGroups": [
+                        {
+                            "mode": "or",
+                            "filters": [
+                                {
+                                    "key": "id",
+                                    "values": entity_data["objectsIds"],
+                                }
+                            ],
+                            "filterGroups": [],
+                        },
+                        access_filter,
+                    ],
+                    "filters": [],
+                }
+
+                entities_sdo = self.helper.api_impersonate.stix_domain_object.list(
+                    filters=export_selection_filter
+                )
+                entities_sco = self.helper.api_impersonate.stix_cyber_observable.list(
+                    filters=export_selection_filter
+                )
+
+                entities_list = entities_sdo + entities_sco
+                for entity in entities_list:
+                    del entity["objectLabelIds"]
+                del entity_data["objectsIds"]
+            if "objectLabelIds" in entity_data:
+                del entity_data["objectLabelIds"]
+            entities_list.append(entity_data)
+            csv_data = self.export_dict_list_to_csv(entities_list)
+            self.helper.connector_logger.info(
+                "Uploading",
+                {
+                    "entity_id": entity_id,
+                    "export_type": export_type,
+                    "file_name": file_name,
+                    "file_markings": file_markings,
+                },
+            )
+            self.helper.api.stix_domain_object.push_entity_export(
+                entity_id, file_name, csv_data, file_markings
+            )
+            self.helper.connector_logger.info(
+                "Export done",
+                {
+                    "entity_type": entity_type,
+                    "entity_id": entity_id,
+                    "export_type": export_type,
+                    "file_name": file_name,
+                    "file_markings": file_markings,
+                },
+            )
+
+        else:  # list export: export_scope = 'query' or 'selection'
+            if export_scope == "selection":
+                list_filters = "selected_ids"
+
+                entity_data_sdo = self.helper.api_impersonate.stix_domain_object.list(
+                    filters=main_filter
+                )
+                entity_data_sco = (
+                    self.helper.api_impersonate.stix_cyber_observable.list(
+                        filters=main_filter
+                    )
+                )
+                entity_data_scr = (
+                    self.helper.api_impersonate.stix_core_relationship.list(
+                        filters=main_filter
+                    )
+                )
+                entity_data_ssr = (
+                    self.helper.api_impersonate.stix_sighting_relationship.list(
+                        filters=main_filter
+                    )
+                )
+
+                entities_list = (
+                    entity_data_sdo
+                    + entity_data_sco
+                    + entity_data_scr
+                    + entity_data_ssr
+                )
+
+                if entities_list is None:
+                    raise ValueError(
+                        "Unable to read/access to the entity, please check that the connector permission. Please note that all export files connectors should have admin permission as they impersonate the user requesting the export to avoir data leak."
+                    )
+
+            else:  # export_scope = 'query'
+                list_params = data["list_params"]
+                list_params_filters = list_params.get("filters")
+                access_filter_content = access_filter.get("filters")
+
+                self.helper.connector_logger.info(
+                    "Exporting list: ",
+                    {
+                        "entity_type": entity_type,
+                        "export_type": export_type,
+                        "file_name": file_name,
+                    },
+                )
+
+                if len(access_filter_content) != 0 and list_params_filters is not None:
+                    export_query_filter = {
+                        "mode": "and",
+                        "filterGroups": [list_params_filters, access_filter],
+                        "filters": [],
+                    }
+                elif len(access_filter_content) == 0:
+                    export_query_filter = list_params_filters
+                else:
+                    export_query_filter = access_filter
+
+                entities_list = self.helper.api_impersonate.stix2.export_entities_list(
+                    entity_type=entity_type,
+                    search=list_params.get("search"),
+                    filters=export_query_filter,
+                    orderBy=list_params["orderBy"],
+                    orderMode=list_params["orderMode"],
+                    getAll=True,
+                )
+                list_filters = json.dumps(list_params)
+
+            if entities_list is not None:
+                csv_data = self.export_dict_list_to_csv(entities_list)
+                self.helper.log_info(
+                    "Uploading: " + entity_type + "/" + export_type + " to " + file_name
+                )
+                if entity_type == "Stix-Cyber-Observable":
+                    self.helper.api.stix_cyber_observable.push_list_export(
+                        entity_id,
+                        entity_type,
+                        file_name,
+                        file_markings,
+                        csv_data,
+                        list_filters,
+                    )
+                elif entity_type == "Stix-Core-Object":
+                    self.helper.api.stix_core_object.push_list_export(
+                        entity_id,
+                        entity_type,
+                        file_name,
+                        file_markings,
+                        csv_data,
+                        list_filters,
+                    )
+                else:
+                    self.helper.api.stix_domain_object.push_list_export(
+                        entity_id,
+                        entity_type,
+                        file_name,
+                        file_markings,
+                        csv_data,
+                        list_filters,
+                    )
+                self.helper.connector_logger.info(
+                    "Export done",
+                    {
+                        "entity_type": entity_type,
+                        "export_type": export_type,
+                        "file_name": file_name,
+                    },
+                )
+            else:
+                raise ValueError("An error occurred, the list is empty")
+
+        return "Export done"
+
+    def _get_indicators(self, indicator_type=None, last_run=None):
+        # now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        filters = {
+            "mode": "and",
+            "filters": [],
+            "filterGroups": [],
+        }
+        if last_run:
+            filters["filters"].append(
+                {"key": "created_at", "values": last_run, "operator": "gt"},
+            )
+        if indicator_type:
+            filters["filters"].append(
+                {"key": "entity_type", "values": indicator_type, "operator": "eq"},
+            )
+        else:
+            self.helper.log_error("No indicator type provided")
+            quit()
+
+        entities_list = self.helper.api_impersonate.stix2.export_entities_list(
+            entity_type=indicator_type,
+            search="google",
+            filters=filters,
+            orderBy="created_at",
+            orderMode="asc",
+            getAll=True,
+        )
+        return entities_list
+
+
+        final_indicators = []
+        data = {"pagination": {"hasNextPage": True, "endCursor": None}}
+        while data["pagination"]["hasNextPage"]:
+            after = data["pagination"]["endCursor"]
+            if after:
+                print("Listing indicators after " + after)
+            data = opencti_api_client.indicator.list(
+                first=50,
+                after=after,
+                customAttributes=custom_attributes,
+                withPagination=True,
+                orderBy="created_at",
+                orderMode="asc",
+            )
+            final_indicators += data["entities"]
+
+        for indicator in final_indicators:
+            print("[" + indicator["created"] + "] " + indicator["id"])
+
+
+
+    def dump_data(self, data, file_path, file_name):
+        csv = self.export_dict_list_to_csv(data)
+        # Check if the directory exists
+        if not os.path.exists(file_path):
+            # If the directory doesn't exist, create it
+            os.makedirs(file_path)
+
+        # Specify the path to the CSV file
+        full_path = os.path.join(file_path, file_name + ".json")
+
+        with open(full_path, "w") as f:
+            json.dump(data, f, indent=4)
+
+        # Write the data to the CSV file
+        full_path = os.path.join(file_path, file_name + ".csv")
+        with open(full_path, "w", newline="") as csvfile:
+            csvfile.write(csv)
+
+    def cleanup(self, data):
+        cleanups = [
+            "id",
+            "standard_id",
+            "parent_types",
+            "spec_version",
+            "objectOrganization",
+            "creators",
+            "createdBy",
+            "objectMarking",
+            "objectLabel",
+            "externalReferences",
+            "indicators",
+            "createdById",
+            "objectMarkingIds",
+            "objectLabelIds",
+            "externalReferencesIds",
+            "indicatorsIds",
+            "mime_type",
+            "hashes",
+            "importFiles",
+            "importFilesIds",
+        ]
+        for x in data:
+            for y in cleanups:
+                if y in x:
+                    del x[y]
+        return data
+
+    def run(self):
+        from pprint import pprint
+        from dateutil.parser import parse
+        import datetime
+
+        while True:
+            # self.helper.connect()
+            self.helper.log_info("Connector started")
+            last_run = parse("2024-05-06").strftime("%Y-%m-%dT%H:%M:%SZ")
+            current_state = self.helper.get_state()
+            entities_list = self._get_indicators(
+                indicator_type="Url", last_run=last_run
+            )
+            entities_list = self.cleanup(entities_list)
+
+            file_path = "data"
+            self.dump_data(entities_list, file_path, "urls")
+
+            self.helper.log_info(
+                f"Connector ended, sleeping for {self.interval/60} minutes"
+            )
+            # self.helper.set_state(
+            #    {
+            #        "last_run": now.timestamp(),
+            #    }
+            # )
+            time.sleep(self.interval)
+
+
+if __name__ == "__main__":
+    try:
+        connectorExportGit = ExportGit()
+        connectorExportGit.run()
+    except Exception as e:
+        print(e)
+        time.sleep(10)
+        sys.exit(0)
